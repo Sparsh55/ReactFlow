5d0e617b1dd89f1bf99f55bae78e3ce4e835a9e9
Sparsh55 <sparsh9654@gmail.com>
2024-10-30 13:22:49 +0530

    loading

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,15 +1,17 @@
-
-import './Loader.css';
+import "./Loader.css";
 
 const Loader = () => {
   return (
-    <div className="creative-loader">
-      <div className="bar"></div>
-      <div className="bar"></div>
-      <div className="bar"></div>
-      <div className="bar"></div>
-      <div className="bar"></div>
-    </div>
+    <>
+      <div className="creative-loader">
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <h4>Loading...</h4>
+      </div>
+    </>
   );
 };
 
