ab7932c2c3230ad725dcea693932dad8244f8e17
Sparsh55 <sparsh9654@gmail.com>
2024-11-02 13:56:34 +0530

    preloader-text-change

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import Navbar from "./components/Navbar.jsx";
 import AboutMe from "./pages/AboutMe.jsx";
 import GetStarted from "./pages/GetStarted.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { useState, useEffect } from 'react';
-import Loader from './components/Loader.jsx';
-import MobileMsg from './components/MobileMsg.jsx';
+import { useState, useEffect } from "react";
+import Loader from "./components/Loader.jsx";
+import MobileMsg from "./components/MobileMsg.jsx";
 
 import "./App.css";
-
 
 export default function App() {
   const [isMobile, setIsMobile] = useState(false);
@@ -19,9 +18,9 @@ export default function App() {
     };
 
     handleResize(); // Initial check
-    window.addEventListener('resize', handleResize); // Check on resize
+    window.addEventListener("resize", handleResize); // Check on resize
 
-    return () => window.removeEventListener('resize', handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   const [loading, setLoading] = useState(true);
@@ -36,9 +35,6 @@ export default function App() {
   if (isMobile) {
     return <MobileMsg />;
   }
-  
-
-  
 
   const router = createBrowserRouter([
     {
@@ -57,26 +53,22 @@ export default function App() {
         {
           path: "/aboutme",
           element: <AboutMe />,
-        }
-      ]
-    }
+        },
+      ],
+    },
   ]);
 
   if (loading) {
     return <Loader />;
   }
 
-
-
   return (
     <>
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <RouterProvider router={router} />
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <RouterProvider router={router} />
       </div>
     </>
-    
   );
-
 
   // return (
   //   <Router>
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,22 @@
 import "./Loader.css";
+import { useState, useEffect } from "react";
 
-const Loader = () => {
+const Loader = () =>{
+  const [message, setMessage] = useState('Fetching data...');
+
+  useEffect(() => {
+    const messages = ['Fetching data...', 'Setting things up...', 'Almost ready...'];
+    let messageIndex = 0;
+
+    const interval = setInterval(() => {
+      messageIndex = (messageIndex + 1) % messages.length;
+      setMessage(messages[messageIndex]);
+    }, 5000); // Change message every 2 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
+
   return (
     <>
       <div className="creative-loader">
@@ -9,7 +25,7 @@ const Loader = () => {
         <div className="bar"></div>
         <div className="bar"></div>
         <div className="bar"></div>
-        <h4>Fatching data, Loading...</h4>
+        <h4>{message}</h4>
       </div>
     </>
   );
